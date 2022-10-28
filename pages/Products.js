78113f7637c1bdcde2901ed30fe2d3d4485ab2ed
rhx1138 <ryanjhendrix@gmail.com>
2022-10-28 07:14:04 -0400

    added components dir with Header, pages dir with cart and products, added routes

---
+++ pages/Products.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Products() {
+    return (
+        <main className="products">
+            <h1>Images go here</h1>
+        </main>
+    )
+}
+
+export default Products
