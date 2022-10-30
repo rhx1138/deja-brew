39c013e2000ad70a62e586ed46ba41bdaad82a5c
rhx1138 <ryanjhendrix@gmail.com>
2022-10-30 11:04:35 -0400

    updated utils index.js

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,3 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import {BrowserRouter as Router} from "react-router-dom"
-
-import App from "./App"
-
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, 
-    document.getElementById("root")
-)
+export function getClass(i) {
+    return 'big'
+}
